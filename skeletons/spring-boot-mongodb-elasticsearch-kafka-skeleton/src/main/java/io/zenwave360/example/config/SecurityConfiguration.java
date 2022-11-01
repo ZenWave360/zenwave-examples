d9a80f06d61806efbbc349983f996f46e3967101
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2022-11-01 20:47:12 +0100

    adds cors configuration (to allow swagger-ui)

--- skeletons/spring-boot-mongodb-elasticsearch-kafka-skeleton/src/main/java/io/zenwave360/example/config/SecurityConfiguration.java
+++ skeletons/spring-boot-mongodb-elasticsearch-kafka-skeleton/src/main/java/io/zenwave360/example/config/SecurityConfiguration.java
@@ -5,6 +5,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
@@ -24,4 +29,14 @@ public class SecurityConfiguration {
         return http.build();
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("Authorization");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
+    }
 }
