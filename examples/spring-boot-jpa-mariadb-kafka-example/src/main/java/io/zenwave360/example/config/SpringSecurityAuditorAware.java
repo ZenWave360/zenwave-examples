9b3d6de20dd6f0662467001e54493533f8f17bc3
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2023-02-10 10:18:11 +0100

    jpa example

--- examples/spring-boot-jpa-mariadb-kafka-example/src/main/java/io/zenwave360/example/config/SpringSecurityAuditorAware.java
+++ examples/spring-boot-jpa-mariadb-kafka-example/src/main/java/io/zenwave360/example/config/SpringSecurityAuditorAware.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 /**
  * Implementation of {@link AuditorAware} based on Spring Security.
  */
-@Component
+@Component("springSecurityAuditorAware")
 public class SpringSecurityAuditorAware implements AuditorAware<String> {
 
     @Override
